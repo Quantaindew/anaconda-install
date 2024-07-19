707d402e67f1c0bd0e9bdc11164e734abf7b4a5b
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-07-20 01:37:06 +0530

    add script

---
+++ script.sh
@@ -0,0 +1,6 @@
+sudo rm -rf ~/anaconda3 && sudo rm -rf ~/.anaconda && sudo rm -rf /root/anaconda && sudo rm -rf ~/anaconda && sudo rm -rf /root/anaconda3 && sudo rm -rf~/.anaconda3 && \
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 && \
+sudo apt install curl && \
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh && \
+bash Anaconda3-2024.06-1-Linux-x86_64.sh && \
+source ~/.bashrc
